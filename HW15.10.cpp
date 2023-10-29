d8c60c5cdf6bd9e53e265f0810cb39317f5194d0
F1eshka <144143888+F1eshka@users.noreply.github.com>
2023-10-29 22:45:39 +0300

    Add files via upload

---
+++ HW15.10.cpp
@@ -0,0 +1,18 @@
+﻿﻿#include <iostream>
+using namespace std;
+
+void Sum(int a)
+{
+    if (a > 0)
+    {
+        cout << a % 10;
+        Sum(a / 10);
+    }
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    Sum(a);
+}
